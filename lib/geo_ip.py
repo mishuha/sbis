3662d2587e3b9bc54c39b9938f1831335aff47b1
mishuha <mishuha@list.ru>
2025-02-03 18:08:18 +0300

    add: requirements.txt

--- lib/geo_ip.py
+++ lib/geo_ip.py
@@ -1,6 +1,8 @@
 import geocoder
 from transliterate import translit
 
+# todo:
+#  check url with translit according to current location
 WEB_CITY = 'Костромская область'
 translit(WEB_CITY, 'ru', reversed=True)
 g = geocoder.ip('me')
